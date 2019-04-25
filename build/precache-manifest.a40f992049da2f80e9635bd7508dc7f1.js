81532d944b697c7c481d53e82af4aac25015db04
williamsii <noar.def@gmail.com>
2019-04-25 23:28:56 +0300

    build and readme

---
+++ build/precache-manifest.a40f992049da2f80e9635bd7508dc7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d3d3bfcaa5f4c35d0598",
+    "url": "/static/js/main.5d9cb662.chunk.js"
+  },
+  {
+    "revision": "0a9fb8d0cc37f01216a5",
+    "url": "/static/js/2.2d7c0e60.chunk.js"
+  },
+  {
+    "revision": "d3d3bfcaa5f4c35d0598",
+    "url": "/static/css/main.9d8511e9.chunk.css"
+  },
+  {
+    "revision": "0a9fb8d0cc37f01216a5",
+    "url": "/static/css/2.5cbe0968.chunk.css"
+  },
+  {
+    "revision": "462193c307c8913a3e0153d2f69196eb",
+    "url": "/index.html"
+  }
+];
